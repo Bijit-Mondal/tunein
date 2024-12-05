84b5ae4ee552f14482339a599cd731ee2dfda39f
Bijit Mondal <bijitmondal3011@rediffmail.com>
2024-12-05 23:29:23 +0530

    tracking added;

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,13 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=VT323&family=Space+Grotesk:wght@300;400;500;600;700&display=swap'
         }
-      ]
+      ],
+      script: [
+        {
+          src: 'https://cdn.splitbee.io/sb.js',
+          async: true
+        }
+      ],
     }
   }
 })
